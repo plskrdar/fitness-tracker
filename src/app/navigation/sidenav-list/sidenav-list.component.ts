c517ab42d43a28d91c9c490a081e6056f9cbabcc
plskrdar <dariusz.skrzypek87@gmail.com>
2021-07-20 00:02:59 +0200

    split logic into separate nav components like header and sidenav

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,22 @@
+import { EventEmitter } from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit {
+
+  @Output() sidenavClose = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClose() {
+    this.sidenavClose.emit();
+  }
+
+}
