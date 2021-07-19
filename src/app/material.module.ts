6209dd65f487121a1cf34a9f4bcc44e2a51ea311
plskrdar <dariusz.skrzypek87@gmail.com>
2021-07-20 00:45:12 +0200

    styling card and added select dropdown with options

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,13 +5,14 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
+import {MatNativeDateModule, MatOptionModule} from "@angular/material/core";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatListModule} from "@angular/material/list";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatCardModule} from "@angular/material/card";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
     imports: [
@@ -27,7 +28,9 @@ import {MatCardModule} from "@angular/material/card";
         MatToolbarModule,
         MatListModule,
         MatTabsModule,
-        MatCardModule
+        MatCardModule,
+        MatSelectModule,
+        MatOptionModule
     ],
     exports: [
         MatSliderModule,
@@ -42,7 +45,9 @@ import {MatCardModule} from "@angular/material/card";
         MatToolbarModule,
         MatListModule,
         MatTabsModule,
-        MatCardModule
+        MatCardModule,
+        MatSelectModule,
+        MatOptionModule
     ]
 })
 export class MaterialModule {
