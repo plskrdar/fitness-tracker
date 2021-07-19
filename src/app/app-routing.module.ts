9e4d0748d6184c1f304735e9ff7149ea92a3909f
plskrdar <dariusz.skrzypek87@gmail.com>
2021-07-20 00:18:16 +0200

    refactored training component place

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {SignupComponent} from "./auth/signup/signup.component";
 import {LoginComponent} from "./auth/login/login.component";
-import {TrainingComponent} from "./training/training/training.component";
+import {TrainingComponent} from "./training/training.component";
 
 const routes: Routes = [
     {path: '', component: WelcomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {CurrentTrainingComponent} from './training/current-training/current-trai
 import {NewTrainingComponent} from './training/new-training/new-training.component';
 import {PastTrainingComponent} from './training/past-training/past-training.component';
 import {WelcomeComponent} from './welcome/welcome.component';
-import {TrainingComponent} from './training/training/training.component';
+import {TrainingComponent} from './training/training.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
