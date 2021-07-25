81c68575c54f369b9ddd31370eb91772f4588789
plskrdar <dariusz.skrzypek87@gmail.com>
2021-07-25 21:37:09 +0200

    added redirection and made logout button works

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import {User} from "./user.model";
 import {AuthData} from "./auth-data.model";
 import {Subject} from "rxjs";
+import {Injectable} from "@angular/core";
+import {Router} from "@angular/router";
 
+@Injectable()
 export class AuthService {
     private user: User;
     authChange = new Subject<boolean>();
+
+    constructor(private router: Router) {
+    }
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: (Math.random() * 10000).toString()
         }
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     login(authData: AuthData) {
@@ -19,12 +25,12 @@ export class AuthService {
             email: authData.email,
             userId: (Math.random() * 10000).toString()
         }
-        this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     logout() {
         this.user = null;
-        this.authChange.next(false);
+        this.authFailure();
     }
 
     getUser() {
@@ -33,5 +39,15 @@ export class AuthService {
 
     isAuth() {
         return this.user !== null;
+    }
+
+    private authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']).then();
+    }
+
+    private authFailure() {
+        this.authChange.next(false);
+        this.router.navigate(['/']).then();
     }
 }
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -30,4 +30,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.sidenavToggle.emit();
     }
 
+    onLogout() {
+        this.authService.logout();
+    }
+
 }
