1aa3cd92c8460e5905bb1b2f37ff8604b2969463
plskrdar <dariusz.skrzypek87@gmail.com>
2021-07-25 22:06:35 +0200

    training service and additional logic

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {TrainingService} from "../training.service";
+import {Exercise} from "../exercise.model";
 
 @Component({
     selector: 'app-new-training',
@@ -7,10 +9,13 @@ import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 })
 export class NewTrainingComponent implements OnInit {
     @Output() trainingStart = new EventEmitter<void>();
-    constructor() {
+    exercises: Exercise[] = [];
+
+    constructor(private trainingService: TrainingService) {
     }
 
     ngOnInit(): void {
+        this.exercises = this.trainingService.getAvailableExercises();
     }
 
     onStartTraining() {
